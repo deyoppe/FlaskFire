1b4a69c2d729bff4559c3fd76a8b80e72cdc8d4e
Deyoppe <deyoppe@gmail.com>
2016-01-17 17:20:37 +0100

    First Commit

---
+++ constants.py
@@ -0,0 +1,5 @@
+from os import path
+
+directory = {}
+directory["app"] = 'app'
+directory["controllers"] = path.join(directory["app"], "controllers")
---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from os import path
+from constants import directory
+import logging
+import importlib
+import glob
+app = Flask(__name__)
+app.debug = True
+
+def run():
+    app.logger.info("Run start")
+    init_app()
+    app.logger.info("Run end")
+    
+def init_logger():
+    app.logger.setLevel(logging.DEBUG)
+    
+def init_app():
+    app.logger.info("App: starting")
+    
+    controller_dir = path.join(directory["controllers"], "*.py")
+    app.logger.debug("Loading all controllers in directory: {:s}".format(controller_dir))
+    
+    controller_files = glob.glob(controller_dir)
+    app.logger.debug("Found controllers: {:s}".format(str(controller_files)))
+    
+    controllers = [ path.basename(f)[:-3] for f in controller_files if path.isfile(f)]
+    for controller in controllers:
+        module_name = "app.controllers.{:s}".format(controller)
+        app.logger.info("Registering controller: {:s}".format(controller))
+        
+        module = getattr(importlib.import_module(module_name), "controller")
+        app.register_blueprint(module, url_prefix="/{:s}".format(controller))
+        app.logger.info("Registration of controller {:s} completed".format(controller))
+        
+    app.run()
+    app.logger.info("App: start completed")
+
+if __name__ == "__main__":  
+    run()
